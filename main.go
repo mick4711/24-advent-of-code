0bc2a568d3b1f5454d9a624a4a0d8251a650b7a8
mick4711 <mick4711@gmail.com>
2024-12-12 20:55:50 +0000

    Day 3.2 solution

--- main.go
+++ main.go
@@ -20,58 +20,57 @@ func main() {
 	}
 
 	s := string(b)
+	ans := getEnabledMuls(s)
+
+	fmt.Println("answer =", ans)
+}
+
+func getEnabledMuls(s string) int {
 	// get slice of do() indices
 	reDo := regexp.MustCompile(`do\(\)`)
 	dos := reDo.FindAllStringIndex(s, -1)
+
 	// get slice of don't() indices
 	reDont := regexp.MustCompile(`don't\(\)`)
 	donts := reDont.FindAllStringIndex(s, -1)
-	// set curr do index = 0
+
+	// set curr do index and value to start of string
 	pDo := -1
 	currDo := []int{0, 0}
+
 	// set curr dont index = 1st dont index
 	pDont := 0
 	currDont := donts[pDont]
 	ans := 0
+
 	// get mulResult of curr do to curr dont
+outerLoop:
 	for {
 		searchString := s[currDo[1]:currDont[0]]
-		log.Println(searchString)
 		ans += getMulResult(searchString)
-		// find next index of do > curr dont and make it curr do
-		pDo++
-		if pDo < len(dos) {
-			currDo = dos[pDo]
-		} else {
-			break
-		}
 
-		// TODO check for currDo > currDont
-		// for currDo[pDo] < currDont[pDont] {
-		// 	pDo++
-		// 	if pDo > len(dos)-1 {
-		// 		break
-		// 	}
-		// }
+		// find next index of do > curr dont and make it curr do
+		for currDo[1] < currDont[0] {
+			pDo++
+			if pDo < len(dos) {
+				currDo = dos[pDo]
+			} else {
+				break outerLoop
+			}
+		}
 
 		// find next index of dont > curr do and make it curr dont
-		pDont++
-		if pDont < len(donts) {
-			currDont = donts[pDont]
-		} else {
-			currDont = []int{len(s) - 1, len(s) - 1}
+		for currDont[1] < currDo[0] {
+			pDont++
+			if pDont < len(donts) {
+				currDont = donts[pDont]
+			} else { // go to end of string
+				currDont = []int{len(s) - 1, len(s) - 1}
+			}
 		}
-
-		// TODO check for currDo > currDont
-		// for currDo[pDo] < currDont[pDont] {
-		// 	pDont++
-		// 	if pDont > len(donts)-1 {
-		// 		break
-		// 	}
-		// }
 	}
 
-	fmt.Println("answer =", ans)
+	return ans
 }
 
 // Parse the numbers out of the string and check for montonic sequence with diff limits
--- main_test.go
+++ main_test.go
@@ -10,13 +10,16 @@ func TestIsSafeReport(t *testing.T) {
 		memory string
 		want   int
 	}{
-		{"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", 48},
+		{"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", 48},                      // clean
+		{"xpqdo()c$mul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", 48},              // preceding do()
+		{"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)xvdon't()aq£$undo()?mul(8,5))", 48},         // trailing don't
+		{"xpqdo()c$mul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)xvdon't()aq£$undo()?mul(8,5))", 48}, // preceding do() and trailing don't
 	}
 
 	for _, test := range tests {
-		got := getMulResult(test.memory)
+		got := getEnabledMuls(test.memory)
 		if got != test.want {
-			t.Errorf("getMulResult(%v), got:%v, want:%v", test.memory, got, test.want)
+			t.Errorf("getEnabledMuls(%v), got:%v, want:%v", test.memory, got, test.want)
 		}
 	}
 }
