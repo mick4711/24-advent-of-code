a923530370e53223b1f3cb1e6b0e30b0374eb196
mick4711 <mick4711@gmail.com>
2024-12-13 19:12:33 +0000

    sync

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
+)
+
+const (
+	searchTerm    = "XMAS"
+	searchTermLen = len(searchTerm)
 )
 
 func main() {
@@ -22,15 +28,181 @@ func main() {
 }
 
 func getXmasCount(file string) int {
+	ans := 0
+	re := regexp.MustCompile(`X`)
 	// split file into lines
 	lines := strings.Split(file, "\n")
 	for i, s := range lines {
 		// find 'x' in each lines
+		xs := re.FindAllStringIndex(s, -1)
+		for _, v := range xs {
+			ans += findW(s, v[0])
+			ans += findE(s, v[0])
+			ans += findS(i, v[0], lines)
+			ans += findN(i, v[0], lines)
+			ans += findNW(i, v[0], lines)
+			ans += findNE(i, v[0], lines)
+			ans += findSW(i, v[0], lines)
+			ans += findSE(i, v[0], lines)
+		}
+
 		// search along 8 directions for 'mas'
-		fmt.Println("line =", i, s)
+		fmt.Println("ans =", i, ans)
 	}
 
-	return len(lines)
+	return ans
+}
+
+func findW(s string, col int) int {
+	if col+searchTermLen > len(s) {
+		return 0
+	}
+
+	var word string
+	for i := 0; i < searchTermLen; i++ {
+		word += string(s[col+i])
+	}
+
+	if word == searchTerm {
+		return 1
+	}
+
+	return 0
+}
+
+func findE(s string, col int) int {
+	if col < searchTermLen-1 {
+		return 0
+	}
+
+	var word string
+	for i := 0; i < searchTermLen; i++ {
+		word += string(s[col-i])
+	}
+
+	if word == searchTerm {
+		return 1
+	}
+
+	return 0
+}
+
+func findS(row, col int, lines []string) int {
+	if row > len(lines)-searchTermLen {
+		return 0
+	}
+
+	var word string
+	for i := 0; i < searchTermLen; i++ {
+		word += string(lines[row+i][col])
+	}
+
+	if word == searchTerm {
+		return 1
+	}
+
+	return 0
+}
+
+func findN(row, col int, lines []string) int {
+	if row < searchTermLen-1 {
+		return 0
+	}
+
+	var word string
+	for i := 0; i < searchTermLen; i++ {
+		word += string(lines[row-i][col])
+	}
+
+	if word == searchTerm {
+		return 1
+	}
+
+	return 0
+}
+
+func findNW(row, col int, lines []string) int {
+	if row < searchTermLen-1 {
+		return 0
+	}
+
+	if col+searchTermLen > len(lines[0]) {
+		return 0
+	}
+
+	var word string
+	for i := 0; i < searchTermLen; i++ {
+		word += string(lines[row-i][col+i])
+	}
+
+	if word == searchTerm {
+		return 1
+	}
+
+	return 0
+}
+
+func findNE(row, col int, lines []string) int {
+	if row < searchTermLen-1 {
+		return 0
+	}
+
+	if col < searchTermLen-1 {
+		return 0
+	}
+
+	var word string
+	for i := 0; i < searchTermLen; i++ {
+		word += string(lines[row-i][col-i])
+	}
+
+	if word == searchTerm {
+		return 1
+	}
+
+	return 0
+}
+
+func findSW(row, col int, lines []string) int {
+	if row > len(lines)-searchTermLen {
+		return 0
+	}
+
+	if col+searchTermLen > len(lines[0]) {
+		return 0
+	}
+
+	var word string
+	for i := 0; i < searchTermLen; i++ {
+		word += string(lines[row+i][col-i])
+	}
+
+	if word == searchTerm {
+		return 1
+	}
+
+	return 0
+}
+
+func findSE(row, col int, lines []string) int {
+	if row > len(lines)-searchTermLen {
+		return 0
+	}
+
+	if col < searchTermLen-1 {
+		return 0
+	}
+
+	var word string
+	for i := 0; i < searchTermLen; i++ {
+		word += string(lines[row+i][col+i])
+	}
+
+	if word == searchTerm {
+		return 1
+	}
+
+	return 0
 }
 
 func getInputFileName() string {
