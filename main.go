96f02d551fddd682fbaa96cbc890962fcb149b93
mick4711 <mick4711@gmail.com>
2024-12-14 00:40:07 +0000

    setup

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
-	"strings"
 )
 
 func main() {
@@ -18,70 +16,12 @@ func main() {
 		panic(err)
 	}
 
-	ans := getXmasCount(string(b))
+	ans := getMiddleSum(string(b))
 	fmt.Println("ans =", ans)
 }
 
-func getXmasCount(file string) int {
-	ans := 0
-	re := regexp.MustCompile(`A`)
-	// split file into lines
-	lines := strings.Split(file, "\n")
-	rowLen := len(lines[0])
-	rowCount := len(lines)
-
-	for row, line := range lines {
-		// eliminate edge rows
-		if row < 1 || row >= rowCount-1 {
-			continue
-		}
-		// find 'A' in each lines
-		xs := re.FindAllStringIndex(line, -1)
-		for _, v := range xs {
-			// eliminate edge cols
-			col := v[0]
-			if col < 1 || col >= rowLen-1 {
-				continue
-			}
-
-			// get the 4 corners
-			tl := string(lines[row-1][col-1])
-			tr := string(lines[row-1][col+1])
-			bl := string(lines[row+1][col-1])
-			br := string(lines[row+1][col+1])
-
-			// search top left for M or S and bottom right for conjugate
-			switch tl {
-			case "M":
-				if br != "S" {
-					continue
-				}
-			case "S":
-				if br != "M" {
-					continue
-				}
-			default:
-				continue
-			}
-
-			// search top right for M or S and bottom left for conjugate
-			switch tr {
-			case "M":
-				if bl != "S" {
-					continue
-				}
-			case "S":
-				if bl != "M" {
-					continue
-				}
-			default:
-				continue
-			}
-
-			ans++
-		}
-	}
-
+func getMiddleSum(file string) int {
+	ans := len(file)
 	return ans
 }
 
--- main_test.go
+++ main_test.go
@@ -4,20 +4,48 @@ import (
 	"testing"
 )
 
-func TestGetXmasCount(t *testing.T) {
+func TestGetMiddleSum(t *testing.T) {
 	tests := []struct {
 		file string
 		want int
 	}{
 		{
-			"MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX", 9,
+			`47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`,
+			143,
 		},
 	}
 
 	for _, test := range tests {
-		got := getXmasCount(test.file)
+		got := getMiddleSum(test.file)
 		if got != test.want {
-			t.Errorf("getXmasCount(%v), got:%v, want:%v", test.file, got, test.want)
+			t.Errorf("getMiddleSum(), got:%v, want:%v", got, test.want)
 		}
 	}
 }
