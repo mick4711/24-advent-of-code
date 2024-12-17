9e1047de3061295178bf9f11efd97f515aca9aa0
mick4711 <mick4711@gmail.com>
2024-12-17 21:51:02 +0000

    Day 5.2 solution

--- main.go
+++ main.go
@@ -34,27 +34,27 @@ func getMiddleSum(file string) int {
 	// iter thru updates check violations
 	for _, update := range strings.Split(rnu[1], "\n") {
 		ns := getNumericSlice(update)
+		ns, needsFixing := fixViolation(ns, bkwds)
 
-		ns, fixed := fixViolation(ns, bkwds)
-		if fixed {
-			if isViolation(ns, bkwds) {
-				panic(fmt.Sprintln("fixed ns is still a violation:", update))
-			}
-
-			middleSum += ns[(len(ns)-1)/2]
+		// didn't need any fix, so move along to next update
+		if !needsFixing {
+			continue
 		}
+
+		// keep fixing until it passes
+		for needsFixing {
+			ns, needsFixing = fixViolation(ns, bkwds)
+		}
+
+		middleSum += ns[(len(ns)-1)/2]
 	}
 
 	return middleSum
 }
 
 // swap number positions when a violation is encountered
-// TODO this ain't fixed
-// 79,49,23,21,24,35,11,76,28,31,89,42,29,26,98,74,41,57,27
-// [23 11 28 31 89 42 98 74 57 27 29 26 79 76 41 21 49 24 35]
-// first pair violates rule 11|23
 func fixViolation(ns []int, bkwds map[int][]int) ([]int, bool) {
-	fixed := false
+	needsFixing := false
 
 	for i, n := range ns {
 		precedings, ok := bkwds[n]
@@ -70,32 +70,13 @@ func fixViolation(ns []int, bkwds map[int][]int) ([]int, bool) {
 			}
 
 			// mark a fixed and swap target with rule value
-			fixed = true
+			needsFixing = true
 			ns[k], ns[j] = ns[j], ns[k]
 			k = j
 		}
 	}
 
-	return ns, fixed
-}
-
-// a violation is when a forward check encounters a bkwds entry
-func isViolation(ns []int, bkwds map[int][]int) bool {
-	for i, n := range ns {
-		precedings, ok := bkwds[n]
-		if !ok {
-			continue
-		}
-
-		// check if any trailing n's are in bkwds
-		for j := i; j < len(ns); j++ {
-			if slices.Contains(precedings, ns[j]) {
-				return true
-			}
-		}
-	}
-
-	return false
+	return ns, needsFixing
 }
 
 func getNumericSlice(update string) []int {
