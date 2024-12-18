0c9345c562b595583e182f94503524f4f63d7eba
mick4711 <mick4711@gmail.com>
2024-12-18 20:01:43 +0000

    navigate and count steps

--- main.go
+++ main.go
@@ -8,6 +8,32 @@ import (
 	"strings"
 )
 
+type (
+	Dir   int
+	Guard struct {
+		row  int
+		col  int
+		path int
+		dir  Dir
+	}
+	Lab struct {
+		obsRows map[int][]int
+		obsCols map[int][]int
+	}
+)
+
+const (
+	Up Dir = iota
+	Right
+	Down
+	Left
+)
+
+var (
+	maxCol int
+	maxRow int
+)
+
 func main() {
 	// set input file name
 	inputFileName := getInputFileName()
@@ -22,69 +48,145 @@ func main() {
 }
 
 func getPathLength(file string) int {
-	pathLength := 0
-	// store obs in row and col maps
-	// find caret and starting direction up
-	const (
-		up = iota
-		right
-		down
-		left
-	)
 	lines := strings.Split(file, "\n")
-	obsRows := make(map[int][]int, len(lines))
-	obsCols := make(map[int][]int, len(lines[0]))
-	startRow := 0
-	startCol := 0
-	dir := up
+
+	// initialise obstructions and guard
+	lab, guard := initialise(lines)
+	// guard.walk finds obs in current dir, update row, col , path , dir, return leave as true or false
+	for {
+		finished := guard.walk(lab)
+		if finished {
+			break
+		}
+	}
+
+	return guard.path
+}
+
+func (guard *Guard) walk(lab Lab) bool {
+	if guard.dir == Up {
+		obs, ok := lab.obsCols[guard.col]
+		if ok {
+			for i := len(obs) - 1; i >= 0; i-- {
+				if obs[i] >= guard.row {
+					continue
+				}
+
+				fmt.Println("hit obs:", obs[i], guard.col)
+				guard.path += guard.row - obs[i] - 1
+				guard.row = obs[i] + 1
+				guard.dir = Right
+
+				return false
+			}
+
+			return true
+		}
+
+		return false
+	}
+
+	if guard.dir == Right {
+		obs, ok := lab.obsRows[guard.row]
+		if ok {
+			for i := 0; i < len(obs); i++ {
+				if guard.col >= obs[i] {
+					continue
+				}
+
+				fmt.Println("hit obs:", guard.row, obs[i])
+				guard.path += obs[i] - guard.col - 1
+				guard.col = obs[i] - 1
+				guard.dir = Down
+
+				return false
+			}
+
+			return true
+		}
+
+		return false
+	}
+
+	if guard.dir == Down {
+		obs, ok := lab.obsCols[guard.col]
+		if ok {
+			for i := 0; i < len(obs); i++ {
+				if guard.row >= obs[i] {
+					continue
+				}
+
+				fmt.Println("hit obs:", obs[i], guard.col)
+				guard.path += obs[i] - guard.row - 1
+				guard.row = obs[i] - 1
+				guard.dir = Left
+
+				return false
+			}
+
+			return true
+		}
+
+		return false
+	}
+
+	if guard.dir == Left {
+		obs, ok := lab.obsRows[guard.row]
+		if ok {
+			for i := len(obs) - 1; i >= 0; i-- {
+				if obs[i] >= guard.col {
+					continue
+				}
+
+				fmt.Println("hit obs:", guard.row, obs[i])
+				guard.path += guard.col - obs[i] - 1
+				guard.col = obs[i] + 1
+				guard.dir = Up
+
+				return false
+			}
+
+			return true
+		}
+
+		return false
+	}
+
+	return true
+}
+
+func initialise(lines []string) (lab Lab, guard Guard) {
+	maxCol = len(lines)
+	maxRow = len(lines[0])
+	lab.obsRows = make(map[int][]int, maxCol)
+	lab.obsCols = make(map[int][]int, maxRow)
 
 	for row, line := range lines {
 		for col, cell := range line {
 			if string(cell) == "#" {
-				_, ok := obsRows[row]
+				_, ok := lab.obsRows[row]
 				if !ok {
-					obsRows[row] = []int{col}
+					lab.obsRows[row] = []int{col}
 				} else {
-					obsRows[row] = append(obsRows[row], col)
+					lab.obsRows[row] = append(lab.obsRows[row], col)
 				}
 
-				_, ok = obsCols[col]
+				_, ok = lab.obsCols[col]
 				if !ok {
-					obsCols[col] = []int{row}
+					lab.obsCols[col] = []int{row}
 				} else {
-					obsCols[col] = append(obsCols[col], row)
+					lab.obsCols[col] = append(lab.obsCols[col], row)
 				}
-
-				pathLength++
 			}
 
+			// find caret and starting direction up
 			if string(cell) == "^" {
-				startRow = row
-				startCol = col
+				guard = Guard{row: row, col: col, path: 1, dir: Up}
 			}
 		}
 	}
 
-	fmt.Println(obsRows)
-	fmt.Println(obsCols)
-	fmt.Println(startRow)
-	fmt.Println(startCol)
-	fmt.Println(dir)
-	// find 1st obstruction in column
-	// change direction right
-	// find obstr in row
-	// change direction down
-	obs, ok := obsCols[startCol]
-	if ok {
-		for i := len(obs) - 1; i >= 0; i-- {
-			if obs[i] < startRow {
-				// turn right and find obs in row
-				fmt.Println("hit obs:", obs[i], startCol)
-			}
-		}
-	}
-
-	return pathLength
+	return lab, guard
 }
 
 func getInputFileName() string {
